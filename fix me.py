a224c3b7258e42b22bba1b40319436c6a3f65199
skoog1o <jalexander2@mail.immaculata.edu>
2025-09-14 15:11:54 -0400

    Add files via upload

---
+++ fix me.py
@@ -0,0 +1,9 @@
+# Fix me
+current_balance = 25
+previous_balance = 20
+account = "Daffy Duck"
+sum42 = 42 == 20 + 22  # This comparison result is now stored in a variable
+total_sum = 2000  # Use snake_case for variable naming consistency
+
+print(f"Your current balance is: {current_balance}")
+print(f"Your previous balance was: {previous_balance}")
